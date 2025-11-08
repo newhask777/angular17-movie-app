21eeb70e9a692f12bf295afcea05caf0c786b496
newhask777 <newhaskellisp@gmail.com>
2025-11-08 11:00:08 +0300

    create input component

--- src/app/pages/test/test.component.ts
+++ src/app/pages/test/test.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
+import { SegmentedControlComponent } from '../../components/segmented-control/segmented-control.component';
+import { SegmentedControlConfig } from '../../interfaces/ui-config/segmented-control.interface';
 
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, SegmentedControlComponent],
   templateUrl: './test.component.html',
   styleUrl: './test.component.scss'
 })
 export class TestComponent {
-
+  segments: SegmentedControlConfig[] = [
+    {
+      name: 'All',
+      active: true
+    },
+    {
+      name: 'Movies',
+      active: false
+    },
+    {
+      name: 'TV Shows',
+      active: false
+    },
+  ]
 }
